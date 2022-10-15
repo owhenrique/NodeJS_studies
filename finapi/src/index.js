1b66af98999acb58ce6ee46f5e8a76f24365d428
owhenrique <me.pauloalmeida@gmail.com>
2022-10-14 21:29:15 -0300

    feat: Create User feature added

--- finapi/src/index.js
+++ finapi/src/index.js
@@ -1,5 +1,34 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid')
 
 const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+app.post('/account', (request, response) => {
+    
+    const { cpf, name } = request.body;
+
+    const customerExists = customers.some(
+        (customer) => customer.cpf === cpf
+    );
+
+    if(customerExists){
+        return response.status(400).json({
+            error: "Customer already exists!"
+        });
+    }
+
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    });
+
+    return response.status(201).send();
+});
 
 app.listen(3000);
