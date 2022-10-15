227feba7983a16e81eaf50a8e932babf0e563bde
owhenrique <me.pauloalmeida@gmail.com>
2022-10-14 22:27:11 -0300

    feat: Create deposite feature added

--- finapi/src/index.js
+++ finapi/src/index.js
@@ -60,6 +60,23 @@ app.get('/statement', customerExistsVerify, (request, response) => {
     return response.status(200).json(
         customer.statement
     );
-})
+});
+
+app.post('/deposite', customerExistsVerify, (request, response) => {
+    const { description, amount } = request.body;
+
+    const { customer } = request;
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+});
 
 app.listen(3000);
